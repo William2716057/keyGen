05aad0d23cb2373531ff70429d60d52d8b1e25c0
William <willspri@gmail.com>
2024-09-21 09:55:44 +1200

    single key printer added

--- keyGen.py
+++ keyGen.py
@@ -1,5 +1,9 @@
 from cryptography.fernet import Fernet
 
+single_key = Fernet.generate_key()
+
+print("Single example key: ", single_key)
+
 def keyGen():
     for i in range(20):
             key = Fernet.generate_key()

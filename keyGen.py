1558c5eeff29b5313b247ddd77e0345ac4560d03
William <willspri@gmail.com>
2024-09-21 09:56:41 +1200

    print statements

--- keyGen.py
+++ keyGen.py
@@ -3,7 +3,7 @@ from cryptography.fernet import Fernet
 single_key = Fernet.generate_key()
 
 print("Single example key: ", single_key)
-
+print("List of keys: ")
 def keyGen():
     for i in range(20):
             key = Fernet.generate_key()
